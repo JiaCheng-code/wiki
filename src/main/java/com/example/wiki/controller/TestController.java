ce1fff702be99d6d2d737f66b8d55169c29bd1ce
JiaCheng-code <675232630@qq.com>
2024-06-21 00:37:19 +0800

    test(数据库):编写测试类验证数据库使用

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,16 +1,23 @@
 package com.example.wiki.controller;
 
+import com.example.wiki.domain.Test;
+import com.example.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class TestController {
     @Value("${test.hello:Test}")
     private String testHello;
+
+    @Resource
+    private TestService testService;
 
     @GetMapping("/test")
     public String test() {
@@ -21,4 +28,10 @@ public class TestController {
     public String testPost(String name) {
         return "Hello World"+name;
     }
+
+    @GetMapping("/test/list")
+    public List<Test> testList() {
+        return testService.list();
+    }
+
 }
