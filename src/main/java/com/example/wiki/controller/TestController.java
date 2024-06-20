5a44a6e5a5ff8d9e1cd2ae72e56da30260606e53
JiaCheng-code <675232630@qq.com>
2024-06-20 22:45:36 +0800

    test(测试):springBoot配置项使用测试

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,14 @@ import java.util.Map;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:Test}")
+    private String testHello;
+
     @GetMapping("/test")
     public String test() {
-        return "Hello World";
+        return "Hello World"+testHello;
     }
+
     @PostMapping("/test/hello")
     public String testPost(String name) {
         return "Hello World"+name;
